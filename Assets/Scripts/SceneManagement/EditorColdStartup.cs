b4b7e829ab1cc44c704e877365b93f803de49795
Sky Rolnick <skymeson@gmail.com>
2021-05-10 17:20:03 -0400

    Merged in scripts from UOP1.

---
+++ Assets/Scripts/SceneManagement/EditorColdStartup.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Allows a "cold start" in the editor, when pressing Play and not passing from the Initialisation scene.
+/// </summary> 
+public class EditorColdStartup : MonoBehaviour
+{
+#if UNITY_EDITOR
+	[SerializeField] private GameSceneSO _thisSceneSO = default;
+	[SerializeField] private GameSceneSO _persistentManagersSO = default;
+	[SerializeField] private AssetReference _notifyColdStartupChannel = default;
+
+	private void Start()
+	{
+		if (!SceneManager.GetSceneByName(_persistentManagersSO.sceneReference.editorAsset.name).isLoaded)
+		{
+			_persistentManagersSO.sceneReference.LoadSceneAsync(LoadSceneMode.Additive, true).Completed += LoadEventChannel;
+		}
+	}
+
+	private void LoadEventChannel(AsyncOperationHandle<SceneInstance> obj)
+	{
+		_notifyColdStartupChannel.LoadAssetAsync<LoadEventChannelSO>().Completed += OnNotifyChannelLoaded;
+	}
+
+	private void OnNotifyChannelLoaded(AsyncOperationHandle<LoadEventChannelSO> obj)
+	{
+		LoadEventChannelSO loadEventChannelSO = (LoadEventChannelSO)_notifyColdStartupChannel.Asset;
+		loadEventChannelSO.RaiseEvent(_thisSceneSO);
+	}
+#endif
+}
---
+++ Assets/Scripts/SceneManagement/StartGame.cs
@@ -0,0 +1,77 @@
+﻿
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.UI;
+
+/// <summary>
+/// This class contains the function to call when play button is pressed
+/// </summary>
+public class StartGame : MonoBehaviour
+{
+	public LoadEventChannelSO onPlayButtonPress;
+	public GameSceneSO locationsToLoad;
+	public bool showLoadScreen;
+
+	//public SaveSystem saveSystem;
+	public Text startText;
+
+	public Button resetSaveDataButton;
+	private bool _hasSaveData;
+
+	private void Start()
+	{
+		//_hasSaveData = saveSystem.LoadSaveDataFromDisk();
+
+		//if (_hasSaveData)
+		//{
+		//	startText.text = "Continue";
+		//	resetSaveDataButton.gameObject.SetActive(true);
+		//}
+		//else
+		//{
+		//	resetSaveDataButton.gameObject.SetActive(false);
+		//}
+	}
+
+	public void OnPlayButtonPress()
+	{
+		if (!_hasSaveData)
+		{
+			//saveSystem.WriteEmptySaveFile();
+			//Start new game
+			onPlayButtonPress.RaiseEvent(locationsToLoad, showLoadScreen);
+		}
+		else
+		{
+			//Load Game
+			StartCoroutine(LoadSaveGame());
+		}
+	}
+
+	public void OnResetSaveDataPress()
+	{
+		_hasSaveData = false;
+		startText.text = "Play";
+		resetSaveDataButton.gameObject.SetActive(false);
+	}
+
+	public IEnumerator LoadSaveGame()
+	{
+		//yield return StartCoroutine(saveSystem.LoadSavedInventory());
+
+		//var locationGuid = saveSystem.saveData._locationId;
+		//var asyncOperationHandle = Addressables.LoadAssetAsync<LocationSO>(locationGuid);
+		//yield return asyncOperationHandle;
+		//if (asyncOperationHandle.Status == AsyncOperationStatus.Succeeded)
+		//{
+		//	LocationSO locationSO = asyncOperationHandle.Result;
+		//	onPlayButtonPress.RaiseEvent(locationSO, showLoadScreen);
+		//}
+
+		Debug.Log("LoadSaveGame not implemented yet. ");
+		yield return null; 
+	}
+}
